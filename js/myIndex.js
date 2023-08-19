4309ecca7414f798cb9b3bf7fdf4341812be5eb2
Imran <webimran2021@gmail.com>
2023-08-19 19:37:11 +0600

    added price calculation

--- js/myIndex.js
+++ js/myIndex.js
@@ -1,3 +1,13 @@
-function handleCLikBtn(target){
-    console.log(target.parentNode.childNodes[1].innerText);
+let total = 0;
+function handleCLikBtn(target) {
+  const selectedItemContainer = document.getElementById("selected-items");
+  const itemName = target.parentNode.childNodes[1].innerText;
+  const li = document.createElement("li");
+  li.innerText = itemName;
+  selectedItemContainer.appendChild(li);
+
+  const price = target.parentNode.childNodes[5].innerText.split(" ")[1];
+  total = parseInt(total) + parseInt(price);
+
+  const firstTotal = (document.getElementById("total").innerText = total);
 }
